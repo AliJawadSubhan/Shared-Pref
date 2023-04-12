1f3348323f9e9a6bf740c08e5d599a6b4886c643
Ali Jawad <jawadsubhan7@gmail.com>
2023-04-12 23:17:47 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,32 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-TextEditingController controller = TextEditingController();
-
-saveDataLocally(BuildContext context) async {
-  if (Form.of(context).validate()) {
-    final preference = await SharedPreferences.getInstance();
-    await preference.setString('Name', controller.text.trim());
-  }
-}
-
 class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadDataLocally();
+  }
+
+  TextEditingController controller = TextEditingController();
+
+  void loadDataLocally() async {
+    final preference = await SharedPreferences.getInstance();
+    if (preference.containsKey('Name')) {
+      controller.text = preference.getString('Name') ?? '';
+    }
+  }
+
+  saveDataLocally(BuildContext context) async {
+    if (Form.of(context).validate()) {
+      final preference = await SharedPreferences.getInstance();
+      await preference.setString('Name', controller.text.trim());
+    }
+  }
+
+  // int age =12;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,54 +57,56 @@ class _MyAppState extends State<MyApp> {
             // key: _formKey,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 50),
-                  const Text(
-                    'Enter your name:',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: controller,
-                    style: TextStyle(fontSize: 18),
-                    decoration: InputDecoration(
-                      hintText: 'John Doe',
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.blue[900]!),
+              child: Builder(builder: (context) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 50),
+                    const Text(
+                      'Enter your name:',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter your name';
-                      } else {
-                        return null;
-                      }
-                    },
-                    onSaved: (value) {},
-                  ),
-                  SizedBox(height: 20),
-                  Align(
-                    alignment: Alignment.center,
-                    child: ElevatedButton(
-                      onPressed: () => saveDataLocally(context),
-                      child: Text('Save Data Locally'),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: controller,
+                      style: TextStyle(fontSize: 18),
+                      decoration: InputDecoration(
+                        hintText: controller.text ?? 'Johhn Doee',
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blue[900]!),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your name';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (value) {},
                     ),
-                  ),
-                ],
-              ),
+                    SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        onPressed: () => saveDataLocally(context),
+                        child: Text('Save Data Locally'),
+                      ),
+                    ),
+                  ],
+                );
+              }),
             ),
           ),
         ),
